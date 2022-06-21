cefd6cf9cb286a272a29f99348f61040b0d39ffa
SnowballSH <derek070211@gmail.com>
2022-06-21 00:18:35 -0500

    Get started by adding a few constants to use later

---
+++ src/com/duck/chess/Board.java
@@ -0,0 +1,6 @@
+package com.duck.chess;
+
+// 0x88 Representation of a chess board
+public class Board {
+    // TODO: Implement the board
+}
---
+++ src/com/duck/chess/Constants.java
@@ -0,0 +1,44 @@
+package com.duck.chess;
+
+// Contains various constants for the board
+public class Constants {
+    /*
+    COLORS
+     */
+    public static final int COLOR_WHITE = 0;
+    public static final int COLOR_BLACK = 1;
+    // COLOR_OPPONENT[color] returns the color of the opponent
+    public static final int[] COLOR_OPPONENT = new int[]{COLOR_BLACK, COLOR_WHITE};
+
+    /*
+    Piece Types
+     */
+    public static final int PIECE_TYPE_NONE = 0;
+    public static final int PIECE_TYPE_PAWN = 1;
+    public static final int PIECE_TYPE_KNIGHT = 2;
+    public static final int PIECE_TYPE_BISHOP = 3;
+    public static final int PIECE_TYPE_ROOK = 4;
+    public static final int PIECE_TYPE_QUEEN = 5;
+    public static final int PIECE_TYPE_KING = 6;
+    // Upper bound of piece types
+    public static final int PIECE_TYPE_ALL = 7;
+
+    /*
+    Pieces
+     */
+    public static final int PIECE_NONE = 0;
+    public static final int PIECE_WHITE_PAWN = 1;
+    public static final int PIECE_WHITE_KNIGHT = 2;
+    public static final int PIECE_WHITE_BISHOP = 3;
+    public static final int PIECE_WHITE_ROOK = 4;
+    public static final int PIECE_WHITE_QUEEN = 5;
+    public static final int PIECE_WHITE_KING = 6;
+    public static final int PIECE_BLACK_PAWN = 7;
+    public static final int PIECE_BLACK_KNIGHT = 8;
+    public static final int PIECE_BLACK_BISHOP = 9;
+    public static final int PIECE_BLACK_ROOK = 10;
+    public static final int PIECE_BLACK_QUEEN = 11;
+    public static final int PIECE_BLACK_KING = 12;
+    // Upper bound of pieces
+    public static final int PIECE_ALL = 13;
+}
