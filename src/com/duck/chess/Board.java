8855f5ef3bd983714a5229934fee66a4583a93dc
dadomman <101952021+dadomman@users.noreply.github.com>
2022-06-22 17:07:53 -0500

    Update Board.java

--- src/com/duck/chess/Board.java
+++ src/com/duck/chess/Board.java
@@ -3,4 +3,12 @@ package com.duck.chess;
 // 0x88 Representation of a chess board
 public class Board {
     // TODO: Implement the board
+    // Board Declaration
+    int [] board = new int[128];
+    //Uses integers to represent one side or the other having the turn to move 0 (white) & 1 (black)
+    int side_to_move = 0;
+    //Next 2 lines represent if the kings of both sides can castle, 1 (castling is available) & 0 (Castling is unavailable)
+    int wht_K_Cstl, wht_Q_Cstl, blk_K_Cstl, blk_Q_Cstl;
+    wht_K_Cstl = wht_Q_Cstl = blk_K_Cstl = blk_Q_Cstl = 1;
+    
 }
