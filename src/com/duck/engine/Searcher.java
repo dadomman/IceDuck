6b561e7020fe2e6e2584bb37e8275a3df90d6413
dadomman <dkeeler@cps.edu>
2022-09-01 20:51:32 -0500

    negamax update so I can work using chromebook

--- src/com/duck/engine/Searcher.java
+++ src/com/duck/engine/Searcher.java
@@ -1,14 +1,20 @@
 package com.duck.engine;
 
 import com.duck.chess.Board;
-
 public class Searcher {
     public int Negamax(Board board, int depth, int alpha, int beta) {
         if (depth == 0) {
             return HCE.evaluateForSTM(board);
         }
+        var moves = board.genLegalMoves();
+        final int mateValue = 999999;
+        int bestValue = -mateValue;
 
-        // ... implement Negamax
+        for(var i = 0; i < moves.size(); i++) {
+            board.makeMove(moves.get(i));
+            bestValue = Math.max(bestValue, - Negamax(board, depth - 1, -beta, -alpha));
+            board.unmakeMove();
+        }
 
         return alpha;
     }
